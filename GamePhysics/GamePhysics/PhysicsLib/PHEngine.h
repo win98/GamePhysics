facd78ed3223a21179ee0d2a0f2c65b80387f207
Sergey <Sergey@192.168.0.100>
2015-02-04 21:00:45 +0000

    Add general header

---
+++ GamePhysics/GamePhysics/PhysicsLib/PHEngine.h
@@ -0,0 +1,14 @@
+//
+//  PHEngine.h
+//  GamePhysics
+//
+//  Created by Sergey on 2/4/15.
+//  Copyright (c) 2015 Sergey. All rights reserved.
+//
+
+#ifndef GamePhysics_PHEngine_h
+#define GamePhysics_PHEngine_h
+
+#include "PHVector.h"
+
+#endif
